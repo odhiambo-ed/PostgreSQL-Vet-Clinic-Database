3b7ef7fb0533b762f1254294367abf56c00e8466
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-31 17:08:51 +0300

    Add Query

--- queries.sql
+++ queries.sql
@@ -10,4 +10,23 @@ SELECT * FROM animals WHERE neutered = 'yes';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <=17.3;
 
+/*How many animals are there*/
+SELECT COUNT(*) FROM animals;
+
+/*How many animals have never tried to escape*/
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+/*What is the average weight of animals*/
+SELECT AVG(weight_kg) FROM animals;
+
+/*Who escapes the most, neutered or not neutered animals*/
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+
+/*What is the minimum and maximum weight of each type of animal*/
+SELECT name, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY name;
+
+/*What is the average number of escape attempts per animal type of those born between 1990 and 2000*/
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31';
+
+
 
